c7c204f8346070a0858b90acf69a9ec7c6969085
sara-abidi <saraabidi428@gmail.com>
2022-08-24 19:50:01 +0100

    Working on understanding recursion

---
+++ Chapter_5/5.7_visualizing_recursion/spiral.py
@@ -0,0 +1,14 @@
+# Basics of turtle graphics
+
+import turtle
+
+def draw_spiral(my_turtle, line_len):
+    if line_len > 0:
+        my_turtle.forward(line_len)
+        my_turtle.right(90)
+        draw_spiral(my_turtle, line_len - 5)
+
+my_turtle = turtle.Turtle()
+my_window = turtle.Screen()
+draw_spiral(my_turtle, 100)
+my_window.exitonclick()
